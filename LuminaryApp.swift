1a60af9e41f9ddb470f5427ddf89785eaedfde8a
Syskey SK <139443204+PRATIKK0709@users.noreply.github.com>
2024-09-18 20:56:01 +0530

    LuminaryApp.swift

---
+++ LuminaryApp.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+@main
+struct ScreenAwakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.hiddenTitleBar)
+        .windowResizability(.contentSize)
+        .commands {
+            CommandGroup(replacing: .newItem) {}
+        }
+    }
+}
